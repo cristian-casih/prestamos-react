3913d9fb8c9c90ec111fc42996597324c692a781
Cristian Casih <cristian.casih@autocity.com.ar>
2020-05-21 15:14:17 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Fragment, useState } from "react"
+import Header from "./components/Header"
+import Formulario from "./components/Formulario"
+import Mensaje from "./components/Mensaje"
+import Resultado from "./components/Resultado"
+import Spinner from "./components/Spinner"
+
+function App() {
+  //Definir el state
+  const [cantidad, guardarCantidad] = useState(0)
+  const [plazo, guardarPlazo] = useState("")
+  const [total, guardarTotal] = useState(0)
+  const [cargando, guardarCargando] = useState(false)
+
+  let componente
+  if (cargando) {
+    componente = <Spinner />
+  } else if (total === 0) {
+    componente = <Mensaje />
+  } else {
+    componente = <Resultado total={total} plazo={plazo} cantidad={cantidad} />
+  }
+
+  return (
+    <Fragment>
+      <Header titulo="Cotizador de Prestamos" />
+      <div className="container">
+        <Formulario
+          cantidad={cantidad}
+          guardarCantidad={guardarCantidad}
+          plazo={plazo}
+          guardarPlazo={guardarPlazo}
+          total={total}
+          guardarTotal={guardarTotal}
+          guardarCargando={guardarCargando}
+        />
+        <div className="mensajes">{componente}</div>
+      </div>
+    </Fragment>
+  )
+}
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+function Header(props) {
+  
+  return <h1>{props.titulo}</h1>
+}
+
+export default Header
